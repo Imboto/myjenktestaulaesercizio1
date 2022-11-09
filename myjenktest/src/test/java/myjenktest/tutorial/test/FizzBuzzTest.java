454ad405dcb7ad79fcce0c677867e24e90c085da
Imboto <gambarellif@gmail.com>
2022-11-09 16:46:53 +0100

    myfirsttest
    test play function

---
+++ myjenktest/src/test/java/myjenktest/tutorial/test/FizzBuzzTest.java
@@ -0,0 +1,26 @@
+package myjenktest.tutorial.test;
+
+
+
+import org.junit.jupiter.api.*;
+
+
+import myjenktest.tutorial.FizzBuzz;
+
+public class FizzBuzzTest {
+	
+	public FizzBuzz fb;
+	
+	@BeforeEach
+	public void setup() {
+		fb=new FizzBuzz();
+	}
+    @DisplayName("Play FizzBuzz with number=1")
+    @Test
+    public void testNumber() {
+    	String risultato=fb.play(1);
+    	//assertEquals (reisultatoOttenuto,risultatoPianificato)- restituisce true o false
+    	Assertions.assertEquals(risultato, "1");
+    }
+    
+}
